dce5f92f81be50ac657e95bf0eb77de8572d1dd4
Sam Raju <34641002+samraju05@users.noreply.github.com>
2021-07-26 12:48:35 +0400

    2 options

--- scripts.js
+++ scripts.js
@@ -1,36 +1,36 @@
-function isSupported(){
-    console.log("NOTIFY","SERVICE CHECK")
+function isSupported() {
+    console.log("NOTIFY", "SERVICE CHECK 2")
     if (!('serviceWorker' in navigator)) {
         // Service Worker isn't supported on this browser, disable or hide UI.
-        console.log("NOTIFY","SERVICE WORKER ","Not available")
+        console.log("NOTIFY", "SERVICE WORKER ", "Not available")
         return false;
     }
 
     if (!('PushManager' in window)) {
         // Push isn't supported on this browser, disable or hide UI.
-        console.log("NOTIFY","PUSH MANAGER","Not available")
+        console.log("NOTIFY", "PUSH MANAGER", "Not available")
         return false;
     }
-    console.log("NOTIFY","SERVICE CHECK","Available")
+    console.log("NOTIFY", "SERVICE CHECK", "Available")
     return true;
 }
 
 async function enableNotification() {
-    console.log("NOTIFY","Enabling Notification")
+    console.log("NOTIFY", "Enabling Notification")
     const result = await Notification.requestPermission();
-    console.log("NOTIFY","Notification Result",result)
+    console.log("NOTIFY", "Notification Result", result)
     if (result == 'granted') {
-        console.log("NOTIFY","Notification Enabled")
+        console.log("NOTIFY", "Notification Enabled")
         await registerSW();
     }
 }
 
 async function registerSW() {
-    console.log("NOTIFY","Registring Service worker")
-    await navigator.serviceWorker.register('./service_worker.js').then(function(registration){
-        console.log("NOTIFY","Registring Service worker success",registration)
-    },function(error){
-        console.log("NOTIFY","Registring Service worker failed",error)
+    console.log("NOTIFY", "Registring Service worker")
+    await navigator.serviceWorker.register('./service_worker.js').then(function(registration) {
+        console.log("NOTIFY", "Registring Service worker success", registration)
+    }, function(error) {
+        console.log("NOTIFY", "Registring Service worker failed", error)
     });
 }
 
@@ -38,18 +38,18 @@ function getSW() {
     return navigator.serviceWorker.getRegistration('./service_worker.js');
 }
 
-function showSimpleNotification(){
-    console.log("NOTIFY","Showing Simple Notification")
+function showSimpleNotification() {
+    console.log("NOTIFY", "Showing Simple Notification")
     const options = {
         body: 'Simple piece of body text.\nSecond line of body text 👍'
     };
-    new Notification("Simple Notification",options)
+    new Notification("Simple Notification", options)
 }
 
 async function showNotification() {
     const reg = await getSW();
-    console.log("NOTIFY","Getting Service Worker",reg)
-    /**** START RNotification ****/
+    console.log("NOTIFY", "Getting Service Worker", reg)
+        /**** START RNotification ****/
     const title = 'Simple Notification';
     const options = {
         body: 'Simple piece of body text.\nSecond line of body text 👍',
@@ -61,7 +61,7 @@ async function showNotification() {
             action: 'action-1'
         }]
     };
-    console.log("NOTIFY","Showing Notification")
+    console.log("NOTIFY", "Showing Notification")
     reg.showNotification(title, options);
     /**** END dirLTRNotification ****/
 }
